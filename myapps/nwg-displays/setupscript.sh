701e306a860cc7f39b166c21042e60b604597012
Doosty <blaz.prah071@gmail.com>
2023-05-08 17:49:37 +0200

    dfghfdsg

--- myapps/nwg-displays/setupscript.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-currentdir=$( dirname -- "$( readlink -f -- "$0"; )"; )
-
-pythoninstallscript="$currentdir/nwg-displays-repo/install.sh"
-if [ ! -x "$pythoninstallscript" ]; then
-    chmod +x "$pythoninstallscript"
-fi
-"$pythoninstallscript"
-
