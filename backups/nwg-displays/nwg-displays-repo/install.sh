3728730aa61a3eb5c71e674adab2388b7e3f28c7
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-05-16 14:24:55 -0700

    big ass update or somethin, removed stuff I don't need

--- backups/nwg-displays/nwg-displays-repo/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-currentdir=$( dirname -- "$( readlink -f -- "$0"; )"; )
-
-python3 "$currentdir"/setup.py install --optimize=1
-cp "$currentdir"/nwg-displays.svg /usr/share/pixmaps/
-cp "$currentdir"/nwg-displays.desktop /usr/share/applications/
