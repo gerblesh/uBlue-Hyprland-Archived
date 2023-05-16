5f1e8baa8f57606d7ac03cb9f9dc730125af4d8a
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-05-16 16:32:29 -0700

    added some config files and /homedir

---
+++ homedir/.config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+function fish_greeting
+
+end
+
+set PATH /usr/bin:/usr/local/bin/:/usr/sbin:/bin:/sbin:/home/user/.local/bin:/home/user/.cargo/bin:$PATH
