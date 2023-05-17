b760f885f55b6320981e079606b503e5210dcb65
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-05-17 13:33:34 -0700

    modified "firstboot" script to copy some homedir shid

--- rootdir/etc/profile.d/ublue-firstboot.sh
+++ rootdir/etc/profile.d/ublue-firstboot.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
 #if test "$(id -u)" -gt "0" && test -d "$HOME"; then
 #    if test ! -e "$HOME"/.config/autostart/ublue-firstboot.desktop; then
 #        mkdir -p "$HOME"/.config/autostart
 #        cp -f /etc/skel.d/.config/autostart/ublue-firstboot.desktop "$HOME"/.config/autostart
 #    fi
 #fi
+
+if test "$(id -u)" -gt "0" && test -d "$HOME"; then
+	cp /etc/homedir/.config/* "$HOME"/.config/
+	cp /etc/homedir/* "$HOME"/
