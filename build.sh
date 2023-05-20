af512f22c05c5bc6417aa2eb2431c7c2e6288ab4
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-05-19 22:12:20 -0700

    removed sddm, attempting to use greetd again...

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ done
 #systemctl enable getty@tty1
 
 #this here has no effect, user has to run it: systemctl set-default graphical.target
-#systemctl enable greetd.service
-systemctl enable ublue-sddm-workaround.service
+systemctl enable greetd.service
+#systemctl enable ublue-sddm-workaround.service
 #systemctl set-default graphical.target
 
